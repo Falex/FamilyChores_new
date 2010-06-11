9f61e178df8b4cc212b347f55d9afd92732878fc
Alexandra <afierascu.mmt-b2008@fh-salzburg.ac.at>
2010-06-11 16:30:47 +0200

    adding ical export from calendar

---
+++ app/controllers/icalfiles_controller.rb
@@ -0,0 +1,38 @@
+class IcalfilesController < ApplicationController
+
+  def index
+    #@chores = Chore.all
+	
+	#my_file = File.new("famcalendar.ics", "w")
+	my_file = File.new("famcalendar.ics", "r+")
+	my_file.write "BEGIN:VCALENDAR" + "\n"
+	
+	my_file.write "METHOD:PUBLISH" + "\n"
+	my_file.write "BEGIN:VEVENT" + "\n"
+	my_file.write "SUMMARY:Staubsaugen" + "- Paul"+ "\n"
+	my_file.write "DTSTART;VALUE=DATE:20100610" + "\n"
+	my_file.write "DTEND;VALUE=DATE:20100611" + "\n"
+	my_file.write "CATEGORIES:Family" + "\n"
+	my_file.write "DESCRIPTION:Paul" + "\n"
+	my_file.write "ATTENDEE;CN=\"Paul\";" + "\n"
+	my_file.write "SEQUENCE:0" + "\n"
+	my_file.write "END:VEVENT" + "\n"
+	my_file.write "END:VCALENDAR" + "\n"
+
+	my_file.write "END:VEVENT" + "\n"
+	my_file.write "END:VCALENDAR"
+	
+
+	
+	
+	
+	#my_file.save
+	
+
+    respond_to do |format|
+	  format.ics {render :ics => @icalfiles}
+      format.html # index.html.erb
+      format.xml  { render :xml => @icalfiles }
+    end
+  end
+end
