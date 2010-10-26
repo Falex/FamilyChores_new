4bf5f61770083d1cec045ac07bae6f90d2e6e139
Alexandra <afierascu.mmt-b2008@fh-salzburg.ac.at>
2010-10-26 23:12:35 +0200

    added browsing through weeks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,102 +1,104 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-
+/*
 jQuery.ajaxSetup({ 'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} })
-02	 
-03	function _ajax_request(url, data, callback, type, method) {
-04	    if (jQuery.isFunction(data)) {
-05	        callback = data;
-06	        data = {};
-07	    }
-08	    return jQuery.ajax({
-09	        type: method,
-10	        url: url,
-11	        data: data,
-12	        success: callback,
-13	        dataType: type
-14	        });
-15	}
-16	 
-17	jQuery.extend({
-18	    put: function(url, data, callback, type) {
-19	        return _ajax_request(url, data, callback, type, 'PUT');
-20	    },
-21	    delete_: function(url, data, callback, type) {
-22	        return _ajax_request(url, data, callback, type, 'DELETE');
-23	    }
-24	});
-25	 
-26	jQuery.fn.submitWithAjax = function() {
-27	  this.unbind('submit', false);
-28	  this.submit(function() {
-29	    $.post(this.action, $(this).serialize(), null, "script");
-30	    return false;
-31	  })
-32	 
-33	  return this;
-34	};
-35	 
-36	//Send data via get if <acronym title="JavaScript">JS</acronym> enabled
-37	jQuery.fn.getWithAjax = function() {
-38	  this.unbind('click', false);
-39	  this.click(function() {
-40	    $.get($(this).attr("href"), $(this).serialize(), null, "script");
-41	    return false;
-42	  })
-43	  return this;
-44	};
-45	 
-46	//Send data via Post if <acronym title="JavaScript">JS</acronym> enabled
-47	jQuery.fn.postWithAjax = function() {
-48	  this.unbind('click', false);
-49	  this.click(function() {
-50	    $.post($(this).attr("href"), $(this).serialize(), null, "script");
-51	    return false;
-52	  })
-53	  return this;
-54	};
-55	 
-56	jQuery.fn.putWithAjax = function() {
-57	  this.unbind('click', false);
-58	  this.click(function() {
-59	    $.put($(this).attr("href"), $(this).serialize(), null, "script");
-60	    return false;
-61	  })
-62	  return this;
-63	};
-64	 
-65	jQuery.fn.deleteWithAjax = function() {
-66	  this.removeAttr('onclick');
-67	  this.unbind('click', false);
-68	  this.click(function() {
-69	    $.delete_($(this).attr("href"), $(this).serialize(), null, "script");
-70	    return false;
-71	  })
-72	  return this;
-73	};
-74	 
-75	//This will "ajaxify" the links
-76	function ajaxLinks(){
-77	    $('.ajaxForm').submitWithAjax();
-78	    $('a.get').getWithAjax();
-79	    $('a.post').postWithAjax();
-80	    $('a.put').putWithAjax();
-81	    $('a.delete').deleteWithAjax();
-82	}
-83	 
-84	$(document).ready(function() {
-85	
-86	// All non-GET requests will add the authenticity token
-87	  // if not already present in the data packet
-88	 $(document).ajaxSend(function(event, request, settings) {
-89	       if (typeof(window.AUTH_TOKEN) == "undefined") return;
-90	       // <acronym title="Internet Explorer 6">IE6</acronym> fix for http://dev.jquery.com/ticket/3155
-91	       if (settings.type == 'GET' || settings.type == 'get') return;
-92	 
-93	       settings.data = settings.data || "";
-94	       settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(window.AUTH_TOKEN);
-95	     });
-96	 
-97	  ajaxLinks();
-98	});
+	 
+	function _ajax_request(url, data, callback, type, method) {
+	    if (jQuery.isFunction(data)) {
+	        callback = data;
+	        data = {};
+	    }
+	    return jQuery.ajax({
+	        type: method,
+	        url: url,
+	        data: data,
+	        success: callback,
+	        dataType: type
+	        });
+	}
+	 
+	jQuery.extend({
+	    put: function(url, data, callback, type) {
+	        return _ajax_request(url, data, callback, type, 'PUT');
+	    },
+	    delete_: function(url, data, callback, type) {
+	        return _ajax_request(url, data, callback, type, 'DELETE');
+	    }
+	});
+	 
+	jQuery.fn.submitWithAjax = function() {
+	  this.unbind('submit', false);
+	  this.submit(function() {
+	    $.post(this.action, $(this).serialize(), null, "script");
+	    return false;
+	  })
+	 
+	  return this;
+	};
+	 
+	//Send data via get if <acronym title="JavaScript">JS</acronym> enabled
+	jQuery.fn.getWithAjax = function() {
+	  this.unbind('click', false);
+	  this.click(function() {
+	    $.get($(this).attr("href"), $(this).serialize(), null, "script");
+	    return false;
+	  })
+	  return this;
+	};
+	 
+	//Send data via Post if <acronym title="JavaScript">JS</acronym> enabled
+	jQuery.fn.postWithAjax = function() {
+	  this.unbind('click', false);
+	  this.click(function() {
+	    $.post($(this).attr("href"), $(this).serialize(), null, "script");
+	    return false;
+	  })
+	  return this;
+	};
+	 
+	jQuery.fn.putWithAjax = function() {
+	  this.unbind('click', false);
+	  this.click(function() {
+	    $.put($(this).attr("href"), $(this).serialize(), null, "script");
+	    return false;
+	  })
+	  return this;
+	};
+	 
+	jQuery.fn.deleteWithAjax = function() {
+	  this.removeAttr('onclick');
+	  this.unbind('click', false);
+	  this.click(function() {
+	    $.delete_($(this).attr("href"), $(this).serialize(), null, "script");
+	    return false;
+	  })
+	  return this;
+	};
+	 
+	//This will "ajaxify" the links
+	function ajaxLinks(){
+	    $('.ajaxForm').submitWithAjax();
+	    $('a.get').getWithAjax();
+	    $('a.post').postWithAjax();
+	    $('a.put').putWithAjax();
+	    $('a.delete').deleteWithAjax();
+	}
+	 
+	$(document).ready(function() {
+	
+	// All non-GET requests will add the authenticity token
+	  // if not already present in the data packet
+	 $(document).ajaxSend(function(event, request, settings) {
+	       if (typeof(window.AUTH_TOKEN) == "undefined") return;
+	       // <acronym title="Internet Explorer 6">IE6</acronym> fix for http://dev.jquery.com/ticket/3155
+	       if (settings.type == 'GET' || settings.type == 'get') return;
+	 
+	       settings.data = settings.data || "";
+	       settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(window.AUTH_TOKEN);
+	     });
+	 
+	  ajaxLinks();
+	});
+	
+*/
